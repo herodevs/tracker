e20020c42ddba5b98622f1467178be70db7e6362
Mike Brocchi <brocco@users.noreply.github.com>
2023-09-14 13:48:49 -0400

    Deprecate this package as the logic has been moved to the herodevs cli

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,2 +1,13 @@
 #!/usr/bin/env node
-export * from './lib';
+
+(async function run() {
+  console.warn(`This package (@herodevs/hd-tracker) has been deprecated`);
+  const messageLines = [
+    '',
+    'It is now available as part of the HeroDevs CLI (@herodevs/cli) via:',
+    'npx @herodevs/cli tracker init',
+    'and',
+    'npx @herodevs/cli tracker run',
+  ];
+  console.log(messageLines.join('\n'));
+})();
